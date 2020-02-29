cdd73d5dd0cdd42da8275c1f54667e4319776e82
Philip <jhasondeleon@gmail.com>
2020-02-28 21:12:51 -0500

    Commit

---
+++ IOSHelloLogin/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  IOSHelloLogin
+//
+//  Created by MacStudent on 2020-02-28.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+@IBOutlet weak var lblWelcome: UILabel!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(viewDidAppear)
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        print(viewWillAppear)
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        print(viewDidDisappear)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        print(viewWillDisappear)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lblWelcome.text = "Welcome to IOS Programming"
+        print(lblWelcome.text)
+    }
+    
+    @IBAction func btnClickMe(_ sender: UIButton)
+    {
+        lblWelcome.text = "Ohh, Don't click again"
+    }
+    
+    @IBAction func btnWelcome2(_ sender: UIButton)
+    {
+        lblWelcome.text = "Welcome to Lambton College"
+    }
+    
+    @IBAction func txtUserName(_ sender: UITextField)
+    {
+        
+    }
+    
+    @IBAction func txtPassWord(_ sender: UITextField)
+    {
+        
+    }
+    
+    
+}
+
