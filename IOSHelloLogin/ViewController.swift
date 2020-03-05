ed91ef2cf2c60554d45d5be5004bc33630fb4c14
philip <jhasondeleon@gmail.com>
2020-03-04 20:18:42 -0500

    commit

--- IOSHelloLogin/ViewController.swift
+++ IOSHelloLogin/ViewController.swift
@@ -10,47 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
-@IBOutlet weak var lblWelcome: UILabel!
+    @IBOutlet weak var txtUserName: UITextField!
+    @IBOutlet weak var txtPassWord: UITextField!
     
-    override func viewDidAppear(_ animated: Bool) {
-        print(viewDidAppear)
-    }
-    override func viewWillAppear(_ animated: Bool) {
-        print(viewWillAppear)
-    }
-    override func viewDidDisappear(_ animated: Bool) {
-        print(viewDidDisappear)
-    }
-    override func viewWillDisappear(_ animated: Bool) {
-        print(viewWillDisappear)
-    }
+//    @IBOutlet weak var lblWelcome: UILabel!
+    
+//    override func viewDidAppear(_ animated: Bool) {
+//        print(viewDidAppear)
+//    }
+//    override func viewWillAppear(_ animated: Bool) {
+//        print(viewWillAppear)
+//    }
+//    override func viewDidDisappear(_ animated: Bool) {
+//        print(viewDidDisappear)
+//    }
+//    override func viewWillDisappear(_ animated: Bool) {
+//        print(viewWillDisappear)
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        lblWelcome.text = "Welcome to IOS Programming"
-        print(lblWelcome.text)
+        
+//        lblWelcome.text = "Welcome to IOS Programming"
+//        print(lblWelcome.text)
     }
     
-    @IBAction func btnClickMe(_ sender: UIButton)
-    {
-        lblWelcome.text = "Ohh, Don't click again"
-    }
+    override func viewWillAppear(_ animated: Bool) {
+    txtUserName.text = ""
+    txtPassWord.text = ""
     
-    @IBAction func btnWelcome2(_ sender: UIButton)
-    {
-        lblWelcome.text = "Welcome to Lambton College"
-    }
+//    @IBAction func btnClickMe(_ sender: UIButton)
+//    {
+//        lblWelcome.text = "Ohh, Don't click again"
+//    }
+//
+//    @IBAction func btnWelcome2(_ sender: UIButton)
+//    {
+//        lblWelcome.text = "Welcome to Lambton College"
+//    }
     
-    @IBAction func txtUserName(_ sender: UITextField)
+        
+    }
+    @IBAction func switchRememberMe(_ sender: UISwitch)
     {
         
     }
-    
-    @IBAction func txtPassWord(_ sender: UITextField)
-    {
-        
-    }
-    
     
 }
 
